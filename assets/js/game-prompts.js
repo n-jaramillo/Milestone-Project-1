67375b332c155c21f7b0a3a6e38e647ec9c1198f
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2024-04-08 11:39:04 -0700

    updated styles and added welcomeDialogue and gamePrompts to js file

---
+++ assets/js/game-prompts.js
@@ -0,0 +1,19 @@
+const welcomeDialogue = `Welcome to the game`
+
+const gamePrompts = {
+    start: {
+        message: `message prompt`,
+        options: {
+            a: {
+                choice: `choice 1`,
+                message: `ending message 1`,
+                gameEnded: true
+            },
+            b: {
+                choice: `choice 2`,
+                message: `ending message 2`,
+                gameEnded: true
+            }
+        }
+    }
+}
